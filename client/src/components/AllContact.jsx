b3028225734e46e240d9cd2afd1db6da127f438a
Saif-Ayari <saifayari312@gmail.com>
2022-11-28 23:05:04 +0100

    tbar9ichet

--- client/src/components/AllContact.jsx
+++ client/src/components/AllContact.jsx
@@ -6,20 +6,19 @@ import Button from 'react-bootstrap/Button';
 // import Update from './Update';
 import { Link } from 'react-router-dom';
 
-// import { useState } from 'react';
-// import axios from 'axios';
 export default function AllContact(props) {
-  // const [newContact, setNewContact] = useState({});
-
-  // const update = (id) => {
-  //   axios.put('http://localhost:8080/update', { id, newContact });
-  // };
   return (
-    <div className="d-flex justify-content-start">
-      <Card style={{ width: '18rem' }}>
+    <center>
+      <Card style={{ width: '18rem', backgroundColor: '#B7E0F2' }}>
         {props.data.map((e, key) => (
           <Card.Body key={key}>
-            {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+            <Card.Img
+            style={{borderRadius: 999 , padding: '3rem'}}
+              variant="top"
+
+              src="https://e7.pngegg.com/pngimages/889/832/png-clipart-google-contacts-mobile-app-contact-manager-app-store-android-application-package-email-miscellaneous-blue.png"
+
+            />
             <Card.Title>{e.name}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{e.phone}</Card.Subtitle>
             <Card.Text>{e.email}</Card.Text>
@@ -45,6 +44,6 @@ export default function AllContact(props) {
           </Card.Body>
         ))}
       </Card>
-    </div>
+    </center>
   );
 }
